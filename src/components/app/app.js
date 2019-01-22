00c1420b7b8c6172b001be43452bb10937ae0cce
afomin <au_fomin@icloud.com>
2019-01-22 12:59:59 +0300

    F commit 1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,6 +11,7 @@ export default class App extends Component {
         return (
             <div>
                 <GitList getData={ this.GithubService.getAllRepositories }/>
+                <div>Hello</div>
             </div>
         )
     }
